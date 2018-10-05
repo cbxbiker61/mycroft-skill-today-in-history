a77fbf30f2f5a0ca460cfcccff376d117a70071e
Kelly Anderson <kelly@xilka.com>
2018-10-05 15:55:22 -0600

    Add all and random capabilities

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ from os.path import dirname
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-import requests
+from mycroft.util import play_wav
+import pickle, os, requests, random, time
 
 __author__ = 'brihopki'
 LOGGER = getLogger(__name__)
-
 
 class TodayHistorySkill(MycroftSkill):
 
@@ -22,12 +22,35 @@ class TodayHistorySkill(MycroftSkill):
         self.register_intent(random_event_intent, self.handle_random_event_intent)
 
     def handle_random_event_intent(self, message):
+        self.speak_dialog('today')
+        time.sleep(0.3)
+
+        if "all" in message.split():
+            for obj in self.getEvents():
+                self.speak(obj)
+        else:
+            self.speak(random.choice(self.getEvents()))
+
+    def speak(self, obj):
+        play_wav(os.path.join(os.path.abspath(os.path.dirname(__file__, 'sounds', 'oneBeep.wav'))))
+        time.sleep(0.3)
+        s = "In " + obj['year'] + ", " + obj['text']
+        self.speak_dialog('event', data{'event': s})
+
+    def getEvents(self):
         url = 'http://history.muffinlabs.com/date'
-        r = requests.get(url)
-        json_output = r.json()
-        output = json_output['data']
-        events = output['Events']
-        self.speak_dialog('event', data={'event': events[0]['text']})
+        cacheFile = "/tmp/TodayInHistoryEvents.cache"
+        cacheSeconds = (60*60*2) # 2 hours
+
+        if not os.path.exists(cacheFile) or (os.path.getmtime(cacheFile) < time.time() - cacheSeconds):
+            r = requests.get(url)
+
+            with open(cacheFile, "wb") as f:
+                # r.json is a dict
+                pickle.dump(r.json()['data']['Events'], f)
+
+        with open(cacheFile, "rb") as f:
+            return pickle.load(f)
 
     def stop(self):
         pass
